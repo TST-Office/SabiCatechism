db023ba5dc617a3504d5b463ff305d8d210253ee
Ifeanyi Nnaemego <josephnaemego@gmail.com>
2023-09-19 22:02:28 +0100

    first commit

---
+++ constants/index.js
@@ -0,0 +1,6 @@
+import theme, { COLORS, SIZES, FONTS } from './theme'
+import images from './images'
+import colors from './colors'
+import data from './data'
+
+export { images, theme, COLORS, SIZES, FONTS, colors, data }
---
+++ screens/index.js
@@ -0,0 +1,21 @@
+import Login from "./Authentication/Login"
+import Signup from "./Authentication/Signup";
+import Welcome from "./Welcome";
+import Home from "./Home/Home";
+import Profile from "./Profile/Profile";
+import EditProfile from "./Profile/EditProfile";
+import Blog from "./Blog/Blog";
+import Messages from "./Messages/Messages";
+import Settings from "./Settings/Settings"
+
+export {
+    Login,
+    Signup,
+    Welcome,
+    Home,
+    Profile,
+    Blog,
+    EditProfile,
+    Messages,
+    Settings
+}
