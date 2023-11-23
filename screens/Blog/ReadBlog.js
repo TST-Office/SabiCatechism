88d561ae75d2f9e8c95da2f67f9cd5e3b197890e
Ifeanyi Nnaemego <josephnaemego@gmail.com>
2023-11-23 15:14:00 +0100

    all video and blog view and back done

--- screens/Blog/ReadBlog.js
+++ screens/Blog/ReadBlog.js
@@ -1,9 +1,18 @@
 import React from "react";
-import { View, StyleSheet, Text, ScrollView, Image } from "react-native";
+import {
+  View,
+  StyleSheet,
+  Text,
+  ScrollView,
+  Image,
+  ActivityIndicator,
+  TouchableOpacity
+} from "react-native";
 import { useRoute } from "@react-navigation/native";
 import { useSelector } from "react-redux";
 import { useNavigation } from "@react-navigation/native";
 import { MaterialIcons } from "@expo/vector-icons";
+
 import {
   DarkBgColors,
   LightBgColors,
@@ -14,6 +23,8 @@ import { formatDistanceToNow, parseISO } from "date-fns";
 import Container from "../../components/Container";
 
 const ReadBlog = () => {
+  const navigation = useNavigation();
+
   const route = useRoute();
   const { blog } = route.params;
   const theme = useSelector((state) => state.theme);
@@ -37,7 +48,7 @@ const ReadBlog = () => {
       height: 400,
       borderRadius: 8,
       marginBottom: 16,
-      objectFit: 'cover'
+      objectFit: "cover",
     },
     title: {
       fontSize: SIZES.h2,
@@ -52,40 +63,71 @@ const ReadBlog = () => {
     },
     author: {
       fontSize: SIZES.body2,
-      color:  theme === "light" ? DarkBgColors.moon : DarkBgColors.bgGray,
+      color: theme === "light" ? DarkBgColors.moon : DarkBgColors.bgGray,
     },
     date: {
       fontSize: SIZES.body2,
-      color:  theme === "light" ? DarkBgColors.moon : DarkBgColors.bgGray,
+      color: theme === "light" ? DarkBgColors.moon : DarkBgColors.bgGray,
     },
     content: {
       fontSize: SIZES.body3,
       lineHeight: 24,
-      color:  theme === "light" ? DarkBgColors.text : COLORS.secondary,
-      marginBottom: 50
+      color: theme === "light" ? DarkBgColors.text : COLORS.secondary,
+      marginBottom: 50,
     },
   });
   return (
     <Container>
-      <ScrollView style={styles.container}>
-        {/* Blog Image */}
-        <Image
-          source={{ uri: `https://api.coinstarr.org/${blog.thumbnail}` }}
-          style={styles.blogImage}
-        />
-
-        {/* Blog Title */}
-        <Text style={styles.title}>{blog.title}</Text>
-
-        {/* Blog Author and Date */}
-        <View style={styles.authorDateContainer}>
-          <Text style={styles.author}>By {blog.author}</Text>
-          <Text style={styles.date}>{formattedDate(blog.created_at)}</Text>
-        </View>
-
-        {/* Blog Content */}
-        <Text style={styles.content}>{blog.description}</Text>
-      </ScrollView>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          marginTop: 30,
+          marginVertical: 40,
+          marginHorizontal: 10,
+        }}
+      >
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <MaterialIcons
+            name="keyboard-arrow-left"
+            size={backButtonSize}
+            color={theme === "light" ? DarkBgColors.text : LightBgColors.text}
+          />
+        </TouchableOpacity>
+        <Text
+          style={{
+            color: theme === "light" ? DarkBgColors.text : LightBgColors.text,
+            fontSize: 25,
+          }}
+        >
+          {blog.title}
+        </Text>
+      </View>
+      {blog ? (
+        <ScrollView style={styles.container}>
+          {/* Blog Image */}
+          <Image
+            source={{ uri: `https://api.coinstarr.org/${blog.thumbnail}` }}
+            style={styles.blogImage}
+          />
+          {/* Blog Title */}
+          <Text style={styles.title}>{blog.title}</Text>
+          {/* Blog Author and Date */}
+          <View style={styles.authorDateContainer}>
+            <Text style={styles.author}>By {blog.author}</Text>
+            <Text style={styles.date}>{formattedDate(blog.created_at)}</Text>
+          </View>
+          {/* Blog Content */}
+          <Text style={styles.content}>{blog.description}</Text>
+        </ScrollView>
+      ) : (
+        <view
+          style={{ flex: 1, justifyContent: "center", alignItems: "center" }}
+        >
+          <ActivityIndicator color={"green"} size={"large"} />
+        </view>
+      )}
     </Container>
   );
 };
--- screens/SingleVideo/PlayVideo.js
+++ screens/SingleVideo/PlayVideo.js
@@ -6,12 +6,22 @@ import { API_URL, COLORS, SIZES } from "../../constants";
 import { DarkBgColors, LightBgColors } from "../../constants/theme";
 import { MaterialIcons } from "@expo/vector-icons";
 import { Video } from "expo-av";
+import { useSelector } from "react-redux";
+import { useNavigation } from "@react-navigation/native";
+
 
 export default function PlayVideo() {
   const route = useRoute();
   const { video } = route.params;
   const videoRef = useRef(null);
   const [isPlaying, setIsPlaying] = useState(false);
+  const backButtonSize = 44;
+  const backButtonMargin = 30;
+  const backButtonTop = Platform.OS === "ios" ? 50 : backButtonMargin;
+  const theme = useSelector((state) => state.theme);
+  const navigation = useNavigation();
+
+
   useEffect(() => {
     playVideo();
   }, []);
@@ -32,12 +42,38 @@ export default function PlayVideo() {
 
   return (
     <Container>
+      <View
+        style={{
+          flexDirection: "row",
+          alignItems: "center",
+          justifyContent: "space-between",
+          marginTop: 30,
+          marginVertical: 10,
+          marginHorizontal: 10,
+        }}
+      >
+        <TouchableOpacity onPress={() => navigation.goBack()}>
+          <MaterialIcons
+            name="keyboard-arrow-left"
+            size={backButtonSize}
+            color={theme === "light" ? DarkBgColors.text : LightBgColors.text}
+          />
+        </TouchableOpacity>
+        <Text
+          style={{
+            color: theme === "light" ? DarkBgColors.text : LightBgColors.text,
+            fontSize: 24,
+          }}
+        >
+          {video?.title}
+        </Text>
+      </View>
       <View style={styles.container}>
         <Video
           ref={videoRef}
           source={{ uri: `https://api.coinstarr.org/${video?.video}` }}
           style={styles.video}
-          resizeMode="contain"
+          resizeMode="cover"
           useNativeControls
         />
 
@@ -53,18 +89,17 @@ export default function PlayVideo() {
 }
 
 const styles = StyleSheet.create({
-    container: {
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center',
-      },
-      video: {
-        width: '100%',
-        aspectRatio: 16 / 9, // You can adjust the aspect ratio based on your video dimensions
-      },
-      controls: {
-        marginTop: 10,
-        flexDirection: 'row',
-        justifyContent: 'center',
-      },
+  container: {
+    justifyContent: "center",
+    alignItems: "center",
+  },
+  video: {
+    width: "100%",
+    aspectRatio: 16 / 9, // You can adjust the aspect ratio based on your video dimensions
+  },
+  controls: {
+    marginTop: 10,
+    flexDirection: "row",
+    justifyContent: "center",
+  },
 });
